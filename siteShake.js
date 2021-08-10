8a22e48ed03535a1667eb0446c5e9fef74607e68
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-10 12:20:39 +0530

    Update siteShake.js

--- siteShake.js
+++ siteShake.js
@@ -1,26 +1,34 @@
-var by1=0;
-var bx1=0;
+
+function generateRandomColor()
+
+{
+
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+    return randomColor;
+
+    //random color will be freshly served
+
+}
+
+
 function rotate(){
     document.body.style.transform = "rotate(45deg)";
     setTimeout(function(){ rotate2()}, 100);
-    setTimeout(function(){ moveD()}, 10000);
+    document.body.style.backgroundColor = generateRandomColor() ;
 }
 function rotate2(){
     document.body.style.transform = "rotate(315deg)";
     setTimeout(function(){ rotate3()}, 100);
+    document.body.style.backgroundColor = generateRandomColor() ;
 }
 function rotate3(){
     document.body.style.transform = "rotate(45deg)";
     setTimeout(function(){ rotate4()}, 100);
+    document.body.style.backgroundColor = generateRandomColor() ;
 }
 function rotate4(){
     document.body.style.transform = "rotate(315deg)";
     setTimeout(function(){ rotate()}, 100);
-}
-function moveD(){
-document.body.style.width = by1;
-document.body.style.height = bx1; 
-by1--;
-bx1--;
-setTimeout(function(){ moveD()}, 100);
+    document.body.style.backgroundColor = generateRandomColor() ;
 }
